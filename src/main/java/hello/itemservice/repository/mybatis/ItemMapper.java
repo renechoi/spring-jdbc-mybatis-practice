11c1cda35c2cb71d7ce10983cf01b5c058469786
renechoi <renechoi90@gmail.com>
2023-03-16 23:03:13 +0900

    test: introduce spring jpa

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -15,6 +15,7 @@ public interface ItemMapper {
     void save(Item item);
 
     void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
+    //여기서는 파라미터가 Long id , ItemUpdateDto updateParam 으로 2개이다. 파라미터가 1개만 있으면 @Param 을 지정하지 않아도 되지만, 파라미터가 2개 이상이면 @Param 으로 이름을 지정해서 파라미터를 구분해야 한다
 
     Optional<Item> findById(Long id);
 
